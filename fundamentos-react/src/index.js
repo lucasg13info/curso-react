b393c2f52b4baeaeddec47b44626b0dbb8c65a3b
Lucas <lucccasestefano_@hotmail.com>
2021-03-02 21:21:50 -0300

    Iniciand JSX - Importando react ára utilizar JSX e colocando as tags HTML no index.js

--- fundamentos-react/src/index.js
+++ fundamentos-react/src/index.js
@@ -1,6 +1,9 @@
 import ReactDOM from'react-dom'
+import React from 'react'
 
 ReactDOM.render(  
-    <div>Olá React!</div>, 
+    <div>
+        <strong>Olá React!</strong>
+    </div>, 
     document.getElementById('root')
 )
