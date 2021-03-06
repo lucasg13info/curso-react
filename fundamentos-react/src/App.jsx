166cd5fdf6b45b6a9277158c3ecffa6488fa39c4
Lucas <lucccasestefano_@hotmail.com>
2021-03-06 20:15:07 -0300

    criando o arquivo App.jsx e passando os componentes para lá

---
+++ fundamentos-react/src/App.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+import Primeiro from './components/basicos/Primeiro';
+import ComParametro from './components/basicos/ComParametro';
+import Fragmento from './components/basicos/Fragmento';
+
+export default function App (props){
+    return (
+        <div id>
+            <h1>Fundamentos React</h1>
+        <Fragmento/>         
+        <ComParametro titulo="Situação do Aluno" 
+            aluno= "Pedro" 
+            nota= {2.3}
+        />
+
+        <ComParametro titulo="Situação do Aluno" 
+            aluno= "Maria" 
+            nota= {9.9} 
+        
+        />
+        <Primeiro></Primeiro>         
+    </div>
+    );
+}
--- fundamentos-react/src/index.js
+++ fundamentos-react/src/index.js
@@ -2,24 +2,9 @@ import './index.css'
 import ReactDOM from'react-dom'
 import React from 'react'
 
-import Primeiro from './components/basicos/Primeiro'
-import ComParametro from './components/basicos/ComParametro'
-import Fragmento from './components/basicos/Fragmento'
+import App from './App'
 
 ReactDOM.render(  
-    <div>
-        <Primeiro></Primeiro>
-        <ComParametro titulo="Situação do Aluno" 
-            aluno= "Pedro" 
-            nota= {2.3} />
-
-        <ComParametro titulo="Situação do Aluno" 
-            aluno= "Maria" 
-            nota= {9.9} />
-
-            <Fragmento/>
-        
-    </div>, 
+   <App/>,
     document.getElementById('root')
-
-    )
+)
