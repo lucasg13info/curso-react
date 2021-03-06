ce16b76c8806c1e74fc458969929e6dcbf33ab8d
Lucas <lucccasestefano_@hotmail.com>
2021-03-06 19:52:00 -0300

    Fragment jsx

--- fundamentos-react/src/index.js
+++ fundamentos-react/src/index.js
@@ -4,6 +4,7 @@ import React from 'react'
 
 import Primeiro from './components/basicos/Primeiro'
 import ComParametro from './components/basicos/ComParametro'
+import Fragmento from './components/basicos/Fragmento'
 
 ReactDOM.render(  
     <div>
@@ -15,6 +16,8 @@ ReactDOM.render(
         <ComParametro titulo="Situação do Aluno" 
             aluno= "Maria" 
             nota= {9.9} />
+
+            <Fragmento/>
         
     </div>, 
     document.getElementById('root')
