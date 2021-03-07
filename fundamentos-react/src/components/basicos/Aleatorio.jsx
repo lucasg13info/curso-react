e50babd1f6e8f029480783e8fde05e689f6090f4
Lucas <lucccasestefano_@hotmail.com>
2021-03-07 19:57:00 -0300

    destructuring na variavel js

--- fundamentos-react/src/components/basicos/Aleatorio.jsx
+++ fundamentos-react/src/components/basicos/Aleatorio.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
 
 export default props =>{
-    const min = props.min
-    const max = props.max
+    const {min , max} = props; 
+
+
     const aleatorio = parseInt(Math.random() *(max - min)) + min
        return(
         
