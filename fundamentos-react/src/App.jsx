f4d47b521cca53d868dbca71c5fa945e16892ce4
Lucas <lucccasestefano_@hotmail.com>
2021-03-07 22:09:28 -0300

    Aplicando cor/ atributos personalizados para cada card e passando como parametro

--- fundamentos-react/src/App.jsx
+++ fundamentos-react/src/App.jsx
@@ -13,22 +13,22 @@ export default () =>(
             <h1>Fundamentos React</h1>
 
         <div className="Cards"> 
-        <Card titulo="#04 -Desafio Aleatório" color="#080">
+        <Card titulo="#04 - Desafio Aleatório" color="#FA6900">
             <Aleatorio min= {1} max={60}/>
         </Card>    
 
-        <Card titulo="#03 -Fragmento">
+        <Card titulo="#03 - Fragmento" color="#E94C6F">
             <Fragmento/> 
         </Card>  
     
-        <Card titulo="#02 -ComParametro">
+        <Card titulo="#02 - ComParametro" color="#E8B71A">
             <ComParametro titulo="Situação do Aluno" 
                 aluno= "Pedro" 
                 nota= {2.3}
             />
         </Card> 
 
-        <Card titulo="#01 - Primeiro Componente">
+        <Card titulo="#01 - Primeiro Componente" color="#588C73">
             <Primeiro></Primeiro> 
         </Card>  
 
