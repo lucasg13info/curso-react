ed61eef0dba56321efbc4315ef6b5bd9ace3e7c3
Lucas <lucccasestefano_@hotmail.com>
2021-03-08 23:16:56 -0300

    Usando o metodo React.cloneElement

--- fundamentos-react/src/App.jsx
+++ fundamentos-react/src/App.jsx
@@ -2,6 +2,7 @@ import './App.css'
 import React from 'react';
 
 import Familia from './components/basicos/Familia'
+import FamiliaMembro from './components/basicos/FamiliaMembro'
 import Card from './components/basicos/layout/Card'
 import Aleatorio from './components/basicos/Aleatorio';
 import Primeiro from './components/basicos/Primeiro';
@@ -16,7 +17,12 @@ export default () =>(
         <div className="Cards"> 
 
         <Card titulo="#05 - Componente com Filhos" color="#00C8F8">
-            <Familia sobrenome="Fereira1"/>
+            <Familia sobrenome="Fereira1">
+                <FamiliaMembro nome="Pedro"/>
+                {/* <FamiliaMembro nome ="Ana"/>
+                <FamiliaMembro nome="Gustavo"/> */}
+            </Familia>
+
         </Card>  
 
         <Card titulo="#04 - Desafio Aleatório" color="#FA6900">
--- fundamentos-react/src/components/basicos/Familia.jsx
+++ fundamentos-react/src/components/basicos/Familia.jsx
@@ -1,12 +1,10 @@
 import React from 'react'
-import FamiliaMembro from'./FamiliaMembro'
+
 
 export default props => {
     return (
         <div>
-            <FamiliaMembro nome="Pedro" sobrenome={props.sobrenome}/>
-            <FamiliaMembro nome ="Ana" {...props}/>
-            <FamiliaMembro nome="Gustavo" sobrenome="Silva"/>
+            {React.cloneElement(props.children)}
         </div>
     )
 }
