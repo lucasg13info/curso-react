a1acd44d4e3192bbc8a52b0c0c5433cd56b2ebc4
Lucas <lucccasestefano_@hotmail.com>
2021-03-10 20:56:20 -0300

    Exercicio repetição, adicionando atributo key no atributo id pois é um dado único

--- fundamentos-react/src/App.jsx
+++ fundamentos-react/src/App.jsx
@@ -1,6 +1,7 @@
 import './App.css'
 import React from 'react';
 
+import ListaAlunos from './components/basicos/repeticao/ListaAlunos'
 import Familia from './components/basicos/Familia'
 import FamiliaMembro from './components/basicos/FamiliaMembro'
 import Card from './components/basicos/layout/Card'
@@ -15,6 +16,11 @@ export default () => (
         <h1>Fundamentos React</h1>
 
         <div className="Cards">
+
+        <Card titulo="#06 - Repetição" color="#FF4C65">
+                <ListaAlunos> </ListaAlunos>
+
+            </Card>
 
             <Card titulo="#05 - Componente com Filhos" color="#00C8F8">
                 <Familia sobrenome="Silva">
