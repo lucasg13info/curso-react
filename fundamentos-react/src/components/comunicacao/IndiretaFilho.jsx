2811bdbccf8c7a3310c54fcbb826cab5a0c863c6
Lucas <lucccasestefano_@hotmail.com>
2021-03-22 22:09:56 -0300

    O pai deve passar uma função como callback e quando ocorrer um evento no filho que é o clique do botão a função é chamada e passado as informações do filho para o pai (Comunicação indireta onde o filho manda informações para o pai)

--- fundamentos-react/src/components/comunicacao/IndiretaFilho.jsx
+++ fundamentos-react/src/components/comunicacao/IndiretaFilho.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 
 export default props => {
+    const cb = props.quandoClicar
     return (
         <div>
             <div>Filho</div>
-            <button onClick={e => props.quandoClicar('João', 53, true)}>
+            <button onClick={e => cb('João', 53, true)}>
                 Fornecer Informações
             </button>
         </div>
--- fundamentos-react/src/components/comunicacao/IndiretaPai.jsx
+++ fundamentos-react/src/components/comunicacao/IndiretaPai.jsx
@@ -2,16 +2,25 @@ import React from 'react'
 import IndiretaFilho from './IndiretaFilho'
 
 export default props => {
+    let nome = '?'
+    let idade = 0;
+    let nerd = false;
 
 
     //Nome idade nerd
-    function forcencerInformacoes(nome, idade, nerd){
-        console.log(nome, idade, nerd)
+    function forcencerInformacoes(nomeParam, idadeParam, nerdParam){
+        nome = nomeParam;
+        idade = idadeParam;
+        nerd = nerdParam;
+
+        console.log(nomeParam, idadeParam, nerdParam)
     }
 
     return (
         <div>
-            <div>Pai</div>
+            <span> { nome } </span>
+            <span> <strong>{ idade }</strong> </span>
+            <span> { nerd ? 'Verdadeiro' : 'Falso'} </span>
             <IndiretaFilho quandoClicar={ forcencerInformacoes }/>
         </div>
     
