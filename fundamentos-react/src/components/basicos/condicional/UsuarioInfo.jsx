0e68557561f5f66357868a98e698aa1a6a45c5c3
Lucas <lucccasestefano_@hotmail.com>
2021-03-22 21:05:24 -0300

    Condicional usando tags

--- fundamentos-react/src/components/basicos/condicional/UsuarioInfo.jsx
+++ fundamentos-react/src/components/basicos/condicional/UsuarioInfo.jsx
@@ -4,14 +4,6 @@ export default props => {
     const usuario = props.usuario || { }
     return (
         <div>
-            {/* <If test={usuario && usuario.nome}>
-                Seja bem vindo <strong>{ usuario.nome }</strong>!
-            </If>
-
-            <If test={usuario || !usuario.nome}>
-                Seja bem vindo <strong>Amigão</strong>!
-            </If> */}
-
             <If test={usuario && usuario.nome}>
                 Seja bem vindo <strong>{ usuario.nome }</strong>!
                 <Else>
