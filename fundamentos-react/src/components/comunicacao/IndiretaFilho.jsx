604dbd38cf091cd77227c0db30a51199652de185
Lucas <lucccasestefano_@hotmail.com>
2021-03-23 21:47:23 -0300

    Componente com estado (hooks) - Gerando um valor verdadeiro ou falso aleadtório

--- fundamentos-react/src/components/comunicacao/IndiretaFilho.jsx
+++ fundamentos-react/src/components/comunicacao/IndiretaFilho.jsx
@@ -2,11 +2,13 @@ import React from 'react'
 
 export default props => {
     const gerarIdade = () => parseInt(Math.random() * (20)) + 50
+    const gerarNerd = () => Math.random() > 0.5 
+
     const cb = props.quandoClicar
     return (
         <div>
             <div>Filho</div>
-            <button onClick={e => cb('João', gerarIdade, true)}>
+            <button onClick={e => cb('João', gerarIdade, gerarNerd)}>
                 Fornecer Informações
             </button>
         </div>
