fddaff772b0e55574a5388bf38621c1c359d0513
Lucas <lucccasestefano_@hotmail.com>
2021-03-23 21:45:34 -0300

    Componente com estado (hooks) - Gerando um valor  aleatório entre 20 e 50

--- fundamentos-react/src/components/comunicacao/IndiretaFilho.jsx
+++ fundamentos-react/src/components/comunicacao/IndiretaFilho.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 
 export default props => {
+    const gerarIdade = () => parseInt(Math.random() * (20)) + 50
     const cb = props.quandoClicar
     return (
         <div>
             <div>Filho</div>
-            <button onClick={e => cb('João', 53, true)}>
+            <button onClick={e => cb('João', gerarIdade, true)}>
                 Fornecer Informações
             </button>
         </div>
