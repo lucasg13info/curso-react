737c2288acd4f0c85ce9088103b139c5adcd6978
Lucas <lucccasestefano_@hotmail.com>
2021-03-23 22:19:16 -0300

    Alterando conteudo do input

--- fundamentos-react/src/components/formulario/Input.jsx
+++ fundamentos-react/src/components/formulario/Input.jsx
@@ -1,15 +1,15 @@
 import "./input.css"
 import React, {useState }from 'react';
 
-export default props => {
-
-
-    const [valor, setValor] = useState('Inicial')
+export default (props) => {
+    const [valor, setValor] = useState('Inicial');
     
-    
+    function quandoMudar(e){
+        setValor(e.target.value)
+    }
     return (
         <div className="Input">
-            <input value={ valor }/>
+            <input value={ valor } onChange={quandoMudar}/>
         </div>
     )
 }
