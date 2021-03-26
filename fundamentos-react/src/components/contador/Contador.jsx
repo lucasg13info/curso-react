d2be5d884c025389cfaa8a9fb75d7079e9b24c2e
Lucas <lucccasestefano_@hotmail.com>
2021-03-26 16:31:22 -0300

    Definindo o passo do contador com o input

--- fundamentos-react/src/components/contador/Contador.jsx
+++ fundamentos-react/src/components/contador/Contador.jsx
@@ -23,13 +23,24 @@ class Contador extends Component{
         }
 
 
-
+        setPasso = (evento) =>{
+            this.setState({
+                passo: +evento.target.value,
+            })
+        }
         
     render() {
         return(
             <div>
                 <h2>Contador</h2>
                 <h3> { this.state.numero } </h3>
+                <div>
+                    <label htmlFor="passoInput">Passo: </label>
+                    <input id="passoInput" type="number"
+                        value={this.state.passo} 
+                        onChange ={this.setPasso}  />
+                
+                </div>
                 <button onClick={this.inc}> + </button>
                 <button onClick={this.dec}> - </button>
             </div>
