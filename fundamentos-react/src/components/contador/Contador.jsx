382645b67ae56eb3d4256393bd39052acd3a2ac6
Lucas <lucccasestefano_@hotmail.com>
2021-03-26 16:25:01 -0300

    Incrementando o decrementando de 5 em 5

--- fundamentos-react/src/components/contador/Contador.jsx
+++ fundamentos-react/src/components/contador/Contador.jsx
@@ -3,20 +3,22 @@ import React, {Component} from 'react'
 class Contador extends Component{
 
         state = {
-            numero: this.props.numeroInicial
+            numero: this.props.numeroInicial || 0,
+            passo: this.props.passoInicial || 5,
+
         };
 
 
     
         inc = () => {
             this.setState({
-                numero: this.state.numero +1
+                numero: this.state.numero +this.state.passo,
             })
         }
 
         dec = () => {
             this.setState({
-                numero: this.state.numero -1
+                numero: this.state.numero -this.state.passo,
             })
         }
 
