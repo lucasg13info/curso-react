690ec4752c8db3d51fa83b773825013f063fd3a5
Lucas <lucccasestefano_@hotmail.com>
2021-03-26 15:56:47 -0300

    Iniciando componente com calss Contador

--- fundamentos-react/src/contador/Contador.jsx
+++ fundamentos-react/src/contador/Contador.jsx
@@ -2,6 +2,14 @@ import React, {Component} from 'react'
 
 class Contador extends Component{
 
+    render() {
+        return(
+            <div>
+                <h2>Contador</h2>
+            </div>
+        )
+    }
+
 }
 
 
