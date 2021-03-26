83856c6377af54c4b14048d3fa9fabd8de236ba7
Lucas <lucccasestefano_@hotmail.com>
2021-03-26 16:10:15 -0300

    Adicionando valores ao contador

--- fundamentos-react/src/App.jsx
+++ fundamentos-react/src/App.jsx
@@ -1,6 +1,7 @@
 import './App.css'
 import React from 'react';
 
+import Contador from './components/contador/Contador'
 import Input from './components/formulario/Input'
 import IndiretaPai from "./components/comunicacao/IndiretaPai"
 import DiretaPai from"./components/comunicacao/DiretaPai";
@@ -22,6 +23,10 @@ export default () => (
         <h1>Fundamentos React</h1>
 
         <div className="Cards">
+        <Card titulo="#12 - Contador" color="#424242">
+                <Contador numeroInicial= {10}/>
+            </Card>
+
         <Card titulo="#11 - Componente controlado (Input)" color="#E45F56">
                 <Input/>
             </Card>
---
+++ fundamentos-react/src/components/contador/Contador.jsx
@@ -0,0 +1,34 @@
+import React, {Component} from 'react'
+
+class Contador extends Component{
+
+        state = {
+            numero: this.props.numeroInicial
+        };
+
+        constructor(props){
+            super(props)
+
+            this.inc = this.inc.bind(this)
+        }
+    
+        inc(){
+            this.setState({
+                numero: this.state.numero +1
+            })
+        }
+    render() {
+        return(
+            <div>
+                <h2>Contador</h2>
+                <h3> { this.state.numero } </h3>
+                <button onClick={this.inc}> + </button>
+            </div>
+            
+        )
+    }
+
+}
+
+
+export default Contador
