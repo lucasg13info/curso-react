5fd089607d7e289c03c39c9f4c6d0e2889118b6a
Lucas <lucccasestefano_@hotmail.com>
2021-04-01 11:43:39 -0300

    Adicionado função conClick aos botoões da calculadora

--- calculadora/src/components/Button.jsx
+++ calculadora/src/components/Button.jsx
@@ -1,5 +1,19 @@
 import React from 'react'
 import './Button.css'
 
-export default props => 
-            <button className="button">{ props.label }</button>
+export default props => {
+    let classes = 'button '
+    classes += props.operation ? 'operation' : ''
+    classes += props.double ? 'double' : ''
+    classes += props.triple ? 'triple' : ''
+
+    return(
+
+     <button 
+        onCllick={e => props.click && props.click(props.label)}
+     classname={classes}>
+        { props.label }
+    </button>
+    )
+}
+           
