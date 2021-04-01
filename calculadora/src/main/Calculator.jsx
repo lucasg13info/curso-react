84ccb9d6eac7ec23d3e45f01346abf558a669c59
Lucas <lucccasestefano_@hotmail.com>
2021-04-01 14:56:39 -0300

    usando eval para realizando os calculos da calculadora

--- calculadora/src/main/Calculator.jsx
+++ calculadora/src/main/Calculator.jsx
@@ -5,7 +5,7 @@ import Button from '../components/Button'
 import Display from '../components/Display'
 
 const intialState = {
-    displaValue: '0',
+    displayValue: '0',
     clearDisplay: false,
     operation: null,
     values: [0,0],
@@ -27,8 +27,29 @@ export default class Calculator extends Component {
     }
 
     setOperation(operation){
-        if(this.state.current ===0){
+        if(this.state.current === 0){
             this.setState({operation, current: 1, clearDisplay: true})
+        }else{
+            const equals = operation === '=' 
+            const currentOperation = this.state.operation
+
+            const values = [...this.state.values]
+            try{
+                values[0] = eval(`${values[0]} ${currentOperation} ${values[1]}`)
+            } catch(e){
+                values[0] = this.state.value[0]
+            }
+            values[1] = 0
+            
+
+            this.setState({
+                displayValue: values[0], 
+                operation: equals ? null : operation,
+                current: equals ? 0 : 1, 
+                clearDisplay: !equals,
+                values 
+            })
+
         }
     }
 
