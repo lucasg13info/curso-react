e38f564cc44071a192ff96c32be5073a9ae86b28
Lucas <lucccasestefano_@hotmail.com>
2021-04-01 00:03:49 -0300

    Usando variaveis no css (:root{)

--- calculadora/src/main/Calculator.jsx
+++ calculadora/src/main/Calculator.jsx
@@ -1,11 +1,18 @@
 import React, {Component} from 'react'
 import './Calculator.css'
 
+import Button from '../components/Button'
+
 export default class Calculator extends Component {
     render() {
         return(
             <div className="calculator">
-                <p>Teste</p>
+                <Button />
+                <Button />
+                <Button />
+                <Button />
+                <Button />
+                
             </div>
         )
     }
