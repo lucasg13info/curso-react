1889bf2c22441a9b165c3cc951a6132612cdd1e6
Lucas <lucccasestefano_@hotmail.com>
2021-04-01 13:04:08 -0300

    criado a função de stados para aplicar a lógica da calculadora

--- calculadora/src/main/Calculator.jsx
+++ calculadora/src/main/Calculator.jsx
@@ -4,6 +4,14 @@ import './Calculator.css'
 import Button from '../components/Button'
 import Display from '../components/Display'
 
+const intialState = {
+    displaValue: '0',
+    clearDisplay: false,
+    operation: null,
+    values: [0,0],
+    current: 0
+}
+
 export default class Calculator extends Component {
 
     constructor(props){
