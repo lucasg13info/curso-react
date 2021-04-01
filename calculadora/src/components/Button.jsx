e0e85498120393928556055f1a98fb7cd632bafd
Lucas <lucccasestefano_@hotmail.com>
2021-04-01 12:34:53 -0300

    Aplicado a função de atribuir valor ao clicar nos botoes

--- calculadora/src/components/Button.jsx
+++ calculadora/src/components/Button.jsx
@@ -3,6 +3,7 @@ import './Button.css'
 
 export default props => {
     let classes = 'button '
+
     classes += props.operation ? 'operation' : ''
     classes += props.double ? 'double' : ''
     classes += props.triple ? 'triple' : ''
@@ -10,10 +11,9 @@ export default props => {
     return(
 
      <button 
-        onCllick={e => props.click && props.click(props.label)}
-     classname={classes}>
+        onClick={e => props.click && props.click(props.label)}
+        className={classes}>
         { props.label }
     </button>
     )
 }
-           
